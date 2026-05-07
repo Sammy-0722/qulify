10ccd847d8c1134d8755a966acc0e8eaaee06bb8
Sammy-0722 <samrana2204@gmail.com>
2026-05-07 20:17:17 +0530

    fix API base URL to include /api

--- react-app/src/services/api.js
+++ react-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://qulify-backend.onrender.com'
+const API_URL = 'https://qulify-backend.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_URL,
